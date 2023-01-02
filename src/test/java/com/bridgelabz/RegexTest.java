289603d9bae349beacf2063cf5faae47cd92653a
Soujanya <soujanya131narlapuram@gmail.com>
2023-01-02 23:04:21 +0530

    "UC2 Commit"

--- src/test/java/com/bridgelabz/RegexTest.java
+++ src/test/java/com/bridgelabz/RegexTest.java
@@ -4,11 +4,26 @@ import org.junit.Test;
 
 public class RegexTest {
     @Test
-    public void testValidateFirstName(){
+    public void testCaseValidateFirstName(){
         boolean valid = Regex.firstNameValidation("soujanya");
         Assertions.assertEquals(false, valid);
 
         valid = Regex.firstNameValidation("Ashok");
         Assertions.assertEquals(true, valid);
+
+        valid = Regex.firstNameValidation("1234");
+        Assertions.assertEquals(false, valid);
+
     }
+    @Test
+    public void testCaseValidateLastName(){
+        boolean valid = Regex.lastNameValidation("narlapuram");
+        Assertions.assertEquals(false, valid);
+
+        valid = Regex.lastNameValidation("Kolipaka");
+        Assertions.assertEquals(true, valid);
+    }
+
+
+
 }
